1dfeac9e1be08312b02663b2ce42afd6f5025618
ToHoangTrung <37842252+ToHoangTrung@users.noreply.github.com>
2022-05-26 21:37:06 +0700

    init project

--- source/src/main/java/com/tqhdl/api/OnlineMarketApplication.java
+++ source/src/main/java/com/tqhdl/api/OnlineMarketApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiBackendForOnlineMarketApplication {
+public class OnlineMarketApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiBackendForOnlineMarketApplication.class, args);
+        SpringApplication.run(OnlineMarketApplication.class, args);
     }
 
 }
