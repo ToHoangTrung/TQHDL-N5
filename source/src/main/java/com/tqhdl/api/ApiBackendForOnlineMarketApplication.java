6f1b47489f40d06abc205d96e633d1cfb63d0c01
ToHoangTrung <37842252+ToHoangTrung@users.noreply.github.com>
2022-05-26 21:31:26 +0700

    init project

---
+++ source/src/main/java/com/tqhdl/api/ApiBackendForOnlineMarketApplication.java
@@ -0,0 +1,13 @@
+package com.tqhdl.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBackendForOnlineMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBackendForOnlineMarketApplication.class, args);
+    }
+
+}
---
+++ source/src/test/java/com/tqhdl/api/ApiBackendForOnlineMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tqhdl.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBackendForOnlineMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
